41c29d3d86d1ba7c02110b93bb1ef09d3246b1d3
Yousef-Alabdulhadi <89356411+Yousef-Alabdulhadi@users.noreply.github.com>
2022-09-18 14:58:43 +0300

    boostrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :articles
-  root 'pages#home'
-  get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index]
+  root to: 'pages#home'
+  # get 'about', to: 'pages#about'
+  resources :articles, only: [:show, :index, :new, :create]
 end
